8b036e3ddb92718f5ec02467a3962127b36caf71
keyamoni05866 <keya05866@gmail.com>
2023-05-18 21:35:56 +0600

    initializing

---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from '../../../assets/doll.png'
+const Navbar = () => {
+    return (
+        <div className="navbar ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+  <div className='ms-10'>
+    <img src={logo} alt=""  className=' w-28'/>
+  </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+    
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end me-6">
+    <a className="btn px-7 bg-purple-600">Login</a>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
